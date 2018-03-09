b57ab3e3a3ea784ce7407ad0f3a9a4f8e2551ab6
杨淮生 <codefan@centit.com>
2018-03-09 17:03:11 +0800

    测试数据库连接池 通过

--- framework-system-cloud/src/main/java/com/centit/framework/frameworksystem/FrameworkSystemCloudApplication.java
+++ framework-system-cloud/src/main/java/com/centit/framework/frameworksystem/FrameworkSystemCloudApplication.java
@@ -2,11 +2,13 @@ package com.centit.framework.frameworksystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class FrameworkSystemApplication {
+@EnableEurekaClient
+public class FrameworkSystemCloudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FrameworkSystemApplication.class, args);
+		SpringApplication.run(FrameworkSystemCloudApplication.class, args);
 	}
 }
--- framework-system-cloud/src/test/java/com/centit/framework/frameworksystem/FrameworkSystemCloudApplicationTests.java
+++ framework-system-cloud/src/test/java/com/centit/framework/frameworksystem/FrameworkSystemCloudApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FrameworkSystemApplicationTests {
+public class FrameworkSystemCloudApplicationTests {
 
 	@Test
 	public void contextLoads() {
