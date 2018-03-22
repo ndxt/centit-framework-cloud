b73915cf06c5e33b44eae3ab75f73c43cf8e5d82
杨淮生 <codefan@centit.com>
2018-03-22 14:05:57 +0800

    删除重复的代码

--- framework-system-cloud/src/main/java/com/centit/framework/config/EnvironmentConfiguation.java
+++ framework-system-cloud/src/main/java/com/centit/framework/config/EnvironmentConfiguation.java
@@ -2,7 +2,6 @@ package com.centit.framework.config;
 
 import com.centit.framework.components.impl.NotificationCenterImpl;
 import com.centit.framework.components.impl.TextOperationLogWriterImpl;
-import com.centit.framework.ip.listener.InstantiationServiceBeanPostProcessor;
 import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.OperationLogWriter;
 import com.centit.framework.model.adapter.PlatformEnvironment;
--- framework-system-cloud/src/main/java/com/centit/framework/ip/service/UserDirectory.java
+++
@@ -1,6 +0,0 @@
-package com.centit.framework.ip.service;
-
-public interface UserDirectory {
-	
-	int synchroniseUserDirectory();
-}
