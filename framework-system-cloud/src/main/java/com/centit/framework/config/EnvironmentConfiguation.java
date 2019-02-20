ce97ce013c12d92e4621fc9788e5faffa837099f
杨淮生 <codefan@centit.com>
2019-02-20 16:59:51 +0800

    修改语法错误

--- framework-system-cloud/src/main/java/com/centit/framework/config/EnvironmentConfiguation.java
+++ framework-system-cloud/src/main/java/com/centit/framework/config/EnvironmentConfiguation.java
@@ -6,9 +6,7 @@ import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.OperationLogWriter;
 import com.centit.framework.model.adapter.PlatformEnvironment;
 import com.centit.framework.security.model.CentitPasswordEncoderImpl;
-import com.centit.framework.security.model.CentitSessionRegistry;
 import com.centit.framework.security.model.CentitUserDetailsService;
-import com.centit.framework.security.model.MemorySessionRegistryImpl;
 import com.centit.framework.system.security.DaoUserDetailsService;
 import com.centit.framework.system.service.impl.DBPlatformEnvironment;
 import org.springframework.context.annotation.Bean;
@@ -43,16 +41,16 @@ public class EnvironmentConfiguation {
         return new HttpSessionCsrfTokenRepository();
     }
 
-    @Bean
+    /*@Bean
     public CentitSessionRegistry centitSessionRegistry() {
         return new MemorySessionRegistryImpl();
     }
-
+    */
 
     @Bean
     public NotificationCenter notificationCenter() {
         NotificationCenterImpl notificationCenter = new NotificationCenterImpl();
-        notificationCenter.initMsgSenders();
+        notificationCenter.initDummyMsgSenders();
         //notificationCenter.registerMessageSender("innerMsg",innerMessageManager);
         return notificationCenter;
     }
