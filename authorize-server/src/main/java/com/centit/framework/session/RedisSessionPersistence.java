52d386a2b7390c6ff5592ee40bfdbd0761b04914
zou_wy <zou_wy@centit.com>
2019-02-22 14:42:18 +0800

    session持久化

--- authorize-server/src/main/java/com/centit/framework/session/RedisSessionPersistence.java
+++ authorize-server/src/main/java/com/centit/framework/session/RedisSessionPersistence.java
@@ -1,14 +1,20 @@
 package com.centit.framework.session;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
+@EnableConfigurationProperties(SessionProperties.class)
 public class RedisSessionPersistence {
+
+    @Autowired
+    private SessionProperties sessionProperties;
 
     @Bean
     public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory("192.168.134.9",6379);
+        return new LettuceConnectionFactory(sessionProperties.getRedis().getHost(),sessionProperties.getRedis().getPort());
     }
 }
