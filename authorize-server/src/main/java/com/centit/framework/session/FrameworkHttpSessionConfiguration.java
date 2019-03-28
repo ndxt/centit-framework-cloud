883f7f246264b4ffcbfadedbae85374b563264ce
杨淮生 <codefan@centit.com>
2019-03-28 10:48:04 +0800

    迁移到 spring boot 2.1.3 (INSTALL SUCCESS)

--- authorize-server/src/main/java/com/centit/framework/session/FrameworkHttpSessionConfiguration.java
+++ authorize-server/src/main/java/com/centit/framework/session/FrameworkHttpSessionConfiguration.java
@@ -1,20 +1,33 @@
 package com.centit.framework.session;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 120)
+@EnableConfigurationProperties(SessionProperties.class)
 public class FrameworkHttpSessionConfiguration {
 
     @Value("${session.strategy.cookie.first:true}")
     private boolean cookieFist;
 
+    @Autowired
+    private SessionProperties sessionProperties;
+
     @Bean
-    public SessionProperties sessionProperties(){
-        return new SessionProperties();
+    public RedisConnectionFactory redisConnectionFactory() {
+        JedisConnectionFactory connection = new JedisConnectionFactory();
+        connection.setPort(sessionProperties.getRedis().getPort());
+        connection.setHostName(sessionProperties.getRedis().getHost());
+        return connection;
+
+        //return new LettuceConnectionFactory(sessionProperties.getRedis().getHost(),sessionProperties.getRedis().getPort());
     }
 }
--- framework-system-cloud/src/main/java/com/centit/framework/config/session/FrameworkHttpSessionConfiguration.java
+++ framework-system-cloud/src/main/java/com/centit/framework/config/session/FrameworkHttpSessionConfiguration.java
@@ -1,20 +1,32 @@
 package com.centit.framework.config.session;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-//EnableSpringHttpSession
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 120)
+@EnableConfigurationProperties(SessionProperties.class)
 public class FrameworkHttpSessionConfiguration {
 
     @Value("${session.strategy.cookie.first:true}")
     private boolean cookieFist;
 
+    @Autowired
+    private SessionProperties sessionProperties;
+
     @Bean
-    public SessionProperties sessionProperties(){
-        return new SessionProperties();
+    public RedisConnectionFactory redisConnectionFactory() {
+        JedisConnectionFactory connection = new JedisConnectionFactory();
+        connection.setPort(sessionProperties.getRedis().getPort());
+        connection.setHostName(sessionProperties.getRedis().getHost());
+        return connection;
+
+        //return new LettuceConnectionFactory(sessionProperties.getRedis().getHost(),sessionProperties.getRedis().getPort());
     }
 }
