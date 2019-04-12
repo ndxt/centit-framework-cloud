4db1b0a77cbde586d71096ffc50d2352b16b80a6
杨淮生 <codefan@centit.com>
2019-04-12 09:50:07 +0800

    测试（config-server）

--- framework-cloud-client/src/main/java/com/centit/framework/cloud/SessionManager.java
+++
@@ -1,7 +0,0 @@
-package com.centit.framework.cloud;
-
-import com.centit.framework.security.model.CentitUserDetails;
-
-public interface SessionManager {
-    CentitUserDetails getUserByToken(String userToken);
-}
--- server-gateway/src/main/java/com/centit/framework/servergateway/ServerGatewayApplication.java
+++ server-gateway/src/main/java/com/centit/framework/servergateway/ServerGatewayApplication.java
@@ -1,7 +1,5 @@
 package com.centit.framework.servergateway;
 
-import com.alibaba.fastjson.parser.Feature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -9,12 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @EnableZuulProxy
