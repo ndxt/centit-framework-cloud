acdc489b189d2114d90967a400f3e3f3e3d3b2a7
杨淮生 <codefan@centit.com>
2019-04-12 11:14:38 +0800

    测试 OAuth 客户端

--- authorize-server/src/main/java/com/centit/framework/system/controller/CurrentSessionController.java
+++ authorize-server/src/main/java/com/centit/framework/system/controller/CurrentSessionController.java
@@ -5,24 +5,29 @@ import com.centit.framework.common.WebOptUtils;
 import com.centit.framework.core.controller.BaseController;
 import com.centit.framework.core.controller.WrapUpResponseBody;
 import com.centit.framework.security.model.CentitUserDetails;
+import com.centit.support.algorithm.CollectionsOpt;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @RestController
 public class CurrentSessionController extends BaseController {
 
     @GetMapping( "/oauthUser")
-    @WrapUpResponseBody
-    public Object getOAuthUserInfo(HttpServletRequest request) {
+    @ResponseBody
+    public Map<String, Object> getOAuthUserInfo(HttpServletRequest request) {
         String token = request.getHeader("Authorization");
         if(token.startsWith("Bearer")){
             token = token.substring(6).trim();
         }
         OAuth2Authentication auth = OAuth2SecurityConfig.resourceServerTokenServices.loadAuthentication(token);
-        return auth.getPrincipal();
+        return CollectionsOpt.createHashMap("user", auth.getUserAuthentication().getPrincipal(),
+            "authorities", AuthorityUtils.authorityListToSet(auth.getUserAuthentication().getAuthorities()));
     }
 
     @GetMapping( "/loginUser")
