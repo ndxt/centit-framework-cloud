c199a15cf3dc8400d536e396f2d0b03483c9dde6
zhangbin <zhangbin@centit.com>
2019-05-28 15:17:02 +0800

    spring cloud 应用监控

---
+++ admin-server/src/main/java/com/centit/framework/adminserver/AdminServerApplication.java
@@ -0,0 +1,16 @@
+package com.centit.framework.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
