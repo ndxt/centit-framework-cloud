e3a6286c0e3721e094a383ec95fec4080e766764
codefan <codefan@sina.com>
2020-03-24 13:22:14 +0800

    整合 cas 登录测试通过

--- server-gateway/src/main/java/com/centit/framework/servergateway/ServerGatewayApplication.java
+++ server-gateway/src/main/java/com/centit/framework/servergateway/ServerGatewayApplication.java
@@ -3,15 +3,21 @@ package com.centit.framework.servergateway;
 import com.alibaba.fastjson.parser.Feature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.centit.framework.core.controller.WrapUpResponseBodyReturnValueHandler;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +27,7 @@ import java.util.List;
 @SpringBootApplication
 @ComponentScan(basePackages="com.centit"/*,
         excludeFilters = @ComponentScan.Filter(value = org.springframework.stereotype.Controller.class)*/)
-public class ServerGatewayApplication extends WebMvcConfigurerAdapter {
+public class ServerGatewayApplication extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
     private FastJsonHttpMessageConverter fastJsonHttpMessageConverter(){
         FastJsonHttpMessageConverter fastJsonHttpMessageConverter =
@@ -46,6 +52,29 @@ public class ServerGatewayApplication extends WebMvcConfigurerAdapter {
         converters.add(fastJsonHttpMessageConverter());
         converters.add(new StringHttpMessageConverter());
     }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        //MvcConfigUtil.setApplicationContext(applicationContext, fastJsonHttpMessageConverter());
+        RequestMappingHandlerAdapter requestMappingHandlerAdapter =
+            applicationContext.getBean(RequestMappingHandlerAdapter.class);
+
+        List<HandlerMethodReturnValueHandler> sortedHandlers = new ArrayList<>(20);
+        List<HandlerMethodReturnValueHandler> defaultHandlers = requestMappingHandlerAdapter.getReturnValueHandlers();
+
+        // 建议都使用框架的这个注解处理，为了提高性能可以放在最前面：
+        sortedHandlers.add(new WrapUpResponseBodyReturnValueHandler(fastJsonHttpMessageConverter()));
+        sortedHandlers.addAll(defaultHandlers);
+        // 下面的代码式 放到 Spring 定义的  Annotation-based 组中 排在 sortedHandlers 后面
+        /*for(HandlerMethodReturnValueHandler handler : defaultHandlers ){
+            sortedHandlers.add(handler);
+            if(handler instanceof RequestResponseBodyMethodProcessor){
+                sortedHandlers.add(new WrapUpResponseBodyReturnValueHandler(jsonHttpMessageConverter));
+            }
+        }*/
+        requestMappingHandlerAdapter.setReturnValueHandlers(sortedHandlers);
+    }
+
 
     public static void main(String[] args) {
         //com.centit.framework.system.config.SystemBeanConfig
