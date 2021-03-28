ee5e1c6b7081a4214025c398b68f00e664066525
zfg <zfg@centit.com>
2021-03-28 21:26:56 +0800

    gateway-pom测试

--- nacos-common/src/main/java/com/centit/framework/common/utils/Constant.java
+++ nacos-common/src/main/java/com/centit/framework/common/utils/Constant.java
@@ -32,9 +32,6 @@ public class Constant {
     /**
      * 菜单类型
      *
-     * @author chenshun
-     * @email sunlightcs@gmail.com
-     * @date 2016年11月15日 下午1:24:29
      */
     public enum MenuType {
         /**
@@ -64,9 +61,6 @@ public class Constant {
     /**
      * 定时任务状态
      *
-     * @author chenshun
-     * @email sunlightcs@gmail.com
-     * @date 2016年12月3日 上午12:07:22
      */
     public enum ScheduleStatus {
         /**
